008a390d0d4a6944f1a1ffab28d845f653e54a19
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2025-03-14 22:35:34 +0200

    test: docker run with mapping

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
-### Temporary database
-### We will replace this dictionary
-database= {
-    'users': {"username": "venky",
-    "password": "admin123"}
+# Temporary database
+# We will replace this dictionary
+database = {
+    'users': {"username": "Mohanad",
+              "password": "admin123"}
 }
-logged_in=False
+logged_in = False
+
 
 @app.route('/', methods=['POST', 'GET'])
 def login():
@@ -19,15 +20,11 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-
         if username == database.get("users").get('username') and password == database.get('users').get('password'):
-
 
             logged_in = True
 
-
             return "<h1>Hi {0}, You are successfully logged in.</h1>".format(username)
         else:
-            
 
             return " <h2>Wrong Credentials! Please <a href=\'/\'>try again</a></h2>"
