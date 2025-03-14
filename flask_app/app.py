f00b65338ae630ec96e1abd4f11c43df7ea93115
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2025-03-15 00:32:18 +0200

    add:  database to flask_app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,30 +1,35 @@
 from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_PASSWORD'] = 'admin123'
+app.config['MYSQL_USER'] = 'testuser'
+app.config['MYSQL_DB'] = 'backend'
+
+app.config['MYSQL_HOST'] = 'backenddb'
+
+db = MySQL(app)
 
 
 # Temporary database
 # We will replace this dictionary
-database = {
-    'users': {"username": "Mohanad",
-              "password": "admin123"}
-}
-
-
-logged_in = False
-
 
 @app.route('/', methods=['POST', 'GET'])
 def login():
+
+    cursor = db.connection.cursor()
+
     if request.method == 'GET':
         return render_template('form.html', title='Sign In')
     else:
         username = request.form.get("username")
         password = request.form.get("password")
 
-        if username == database.get("users").get('username') and password == database.get('users').get('password'):
+        status = cursor.execute(
+            '''SELECT username FROM `users` WHERE `username`="{0}" and `password`="{1}"'''.format(username, password))
 
-            logged_in = True
+        if status:
 
             return "<h1>Hi {0}, You are successfully logged in.</h1>".format(username)
         else:
